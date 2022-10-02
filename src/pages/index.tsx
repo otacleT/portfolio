9b63d6ea815ee2425fa1298e6e2bfeb21cc9f9ef
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 16:34:30 +0900

    ♻️ Githubコンポーネント

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Bio } from "src/components/Bio";
-import { Repository } from "src/components/Repository";
+import { Github } from "src/components/Github";
 import { Skills } from "src/components/Skills";
-import { fetcher } from "src/lib/fetcher";
-import useSWR from "swr";
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR(
-    "https://api.github.com/users/otacleT/repos?sort=created&direction=desc&per_page=3",
-    fetcher
-  );
-  console.log({ data, error });
-  if (!error && !data) {
-    return <div>ローディング中</div>;
-  }
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-  if (data.length === 0) {
-    return <div>データは空です</div>;
-  }
-
   return (
     <div className="max-w-xl mx-auto px-4 pt-3">
       <Head>
@@ -50,20 +33,7 @@ const Home: NextPage = () => {
       </ul>
       <Bio />
       <Skills />
-      <section className="section-wrap">
-        <h2 className="section-title">GIthub</h2>
-        {data.map((data: any) => (
-          <Repository
-            key={data.id}
-            name={data.name}
-            description={data.description ?? "No descroption"}
-            languagesUrl={data.languages_url}
-          />
-        ))}
-        <button className="w-[200px] h-10 flex items-center justify-center text-white bg-black rounded-md mx-auto mt-7">
-          View on Github
-        </button>
-      </section>
+      <Github />
     </div>
   );
 };
