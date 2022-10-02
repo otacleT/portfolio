385bf18fc1704d96b65152e6f17dd62f446d15ce
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 20:30:22 +0900

    ✨ Twitterコンポーネント

---
+++ src/components/Twitter/Twitter.tsx
@@ -0,0 +1,8 @@
+export const Twitter = () => {
+  return (
+    <section className="section-wrap">
+      <h2 className="section-title">Twitter</h2>
+      <p className="text-sm text-center my-10">準備中</p>
+    </section>
+  );
+};
