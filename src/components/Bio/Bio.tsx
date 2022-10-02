e260dfe22ddfec6589036948590b2b13e3cf9fe9
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 22:37:37 +0900

    💄 経歴のテキストサイズ

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -2,7 +2,7 @@ export const Bio = () => {
   return (
     <section className="section-wrap mt-28">
       <h2 className="section-title">Bio</h2>
-      <ul className="[&>li]:text-sm mt-3">
+      <ul className="[&>li]:text-sm sm:[&>li]:text-base mt-3">
         <li>
           <span className="font-semibold pr-3">2022.3</span>
           九州工業大学情報工学部　卒業
