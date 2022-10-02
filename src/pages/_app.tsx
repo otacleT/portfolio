1ed297c45174aa8dcef59ef0800dcd6f42a9939f
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 20:28:59 +0900

    ✨ Footerコンポーネント

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "src/components/Header";
 import { MantineProvider } from "@mantine/core";
+import { Footer } from "src/components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </MantineProvider>
   );
 }
