b299182c2fb52fe0216a46db17c9f8fee0f96fb6
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 22:03:18 +0900

    💄 worksページのレスポンシブ対応

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -4,102 +4,102 @@ import Link from "next/link";
 
 const Works: NextPage = () => {
   return (
-    <div className="max-w-xl mx-auto px-4 pt-3">
+    <div className="max-w-xl mx-auto px-4 pt-5">
       <h2 className="section-title">Works</h2>
       <h3 className="section-subTitle">- Decentralized app</h3>
       <div className="flex flex-wrap justify-between items-start">
         <Link href="/">
-          <a className="block mt-3">
+          <a className="w-full sm:w-[calc(50%-5px)] block mt-3">
             <div className="w-full aspect-video rounded-md overflow-hidden relative border border-[#E6F0EB]">
               <Image
                 className="object-cover"
                 src="/gourmet-top.png"
-                width={400}
-                height={250}
+                width={600}
+                height={360}
               />
             </div>
-            <h4 className="text-[14px] font-semibold text-center mt-3">
+            <h4 className="text-[14px] sm:text-[15px] font-semibold text-center mt-3">
               信頼度に応じた飲食店評価アプリ
             </h4>
-            <p className="text-xs text-center mt-1">
+            <p className="text-xs sm:text-sm text-center mt-1">
               ブロックチェーン技術を用いたグルメサービスです。
             </p>
           </a>
         </Link>
       </div>
       <h3 className="section-subTitle mt-8">- Web app</h3>
-      <div className="flex flex-wrap justify-between items-start">
+      <div className="flex flex-wrap justify-between items-start space-y-6 sm:space-y-0 mt-3">
         <Link href="/">
-          <a className="block mt-3">
+          <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative border border-[#E6F0EB]">
               <Image
                 className="object-cover"
                 src="/todo-top.png"
-                width={400}
-                height={250}
+                width={600}
+                height={360}
               />
             </div>
-            <h4 className="text-[14px] font-semibold text-center mt-3">
+            <h4 className="text-[14px] sm:text-[15px] font-semibold text-center mt-3">
               Colorful Todo App
             </h4>
-            <p className="text-xs text-center mt-1">
+            <p className="text-xs sm:text-sm text-center mt-1">
               UI/UXにこだわったTodoアプリです。
             </p>
           </a>
         </Link>
         <Link href="/">
-          <a className="block mt-6">
+          <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative border border-[#E6F0EB]">
               <Image
                 className="object-cover"
                 src="/twitter-top.png"
-                width={400}
-                height={250}
+                width={600}
+                height={360}
               />
             </div>
-            <h4 className="text-[14px] font-semibold text-center mt-3">
+            <h4 className="text-[14px] sm:text-[15px] font-semibold text-center mt-3">
               No Spoilers Twitter!!!
             </h4>
-            <p className="text-xs text-center mt-1">
+            <p className="text-xs sm:text-sm text-center mt-1">
               Twitterによるネタバレを防ぐアプリです。
             </p>
           </a>
         </Link>
       </div>
       <h3 className="section-subTitle mt-8">- Web page</h3>
-      <div className="flex flex-wrap justify-between items-start">
+      <div className="flex flex-wrap justify-between items-start space-y-6 sm:space-y-0 mt-3">
         <Link href="/">
-          <a className="block mt-3">
+          <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative border border-[#E6F0EB]">
               <Image
                 className="object-cover"
                 src="/kyuko-top.JPG"
-                width={400}
-                height={250}
+                width={600}
+                height={360}
               />
             </div>
-            <h4 className="text-[14px] font-semibold text-center mt-3">
+            <h4 className="text-[14px] sm:text-[15px] font-semibold text-center mt-3">
               九州工科自動車専門学校のHP
             </h4>
-            <p className="text-xs text-center mt-1">
+            <p className="text-xs sm:text-sm text-center mt-1">
               フロント部分のコーディングを行いました。
             </p>
           </a>
         </Link>
         <Link href="/">
-          <a className="block mt-7">
+          <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative border border-[#E6F0EB]">
               <Image
                 className="object-cover"
                 src="/fukupoke-top.JPG"
-                width={400}
-                height={250}
+                width={600}
+                height={360}
               />
             </div>
-            <h4 className="text-[14px] font-semibold text-center mt-3">
+            <h4 className="text-[14px] sm:text-[15px] font-semibold text-center mt-3">
               福祉のポータルサイト「ふくポケ」のHP
             </h4>
-            <p className="text-xs text-center mt-1">
+            <p className="text-xs sm:text-sm text-center mt-1">
               フロント部分のコーディングを行いました。
             </p>
           </a>
