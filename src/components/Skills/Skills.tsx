3dc2d6295f55e62a8c3c1ddae711145262b3d316
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 22:38:28 +0900

    💄 技術スタックの画像サイズ調整

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,45 +4,45 @@ export const Skills = () => {
   return (
     <section className="section-wrap">
       <h2 className="section-title">Skills</h2>
-      <ul className="flex items-center space-x-1 space-y-1 mt-3 flex-wrap [&>li]:w-8 [&>li]:h-8">
+      <ul className="flex items-center space-x-1 sm:space-x-2 space-y-1 sm:space-y-2 mt-3 flex-wrap [&>li]:w-8 sm:[&>li]:w-10 [&>li]:h-8 sm:[&>li]:h-10">
         <li>
-          <Image src="/skill-html5.svg" width={32} height={32} />
+          <Image src="/skill-html5.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-css3.svg" width={32} height={32} />
+          <Image src="/skill-css3.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-sass.svg" width={32} height={32} />
+          <Image src="/skill-sass.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-php.svg" width={32} height={32} />
+          <Image src="/skill-php.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-js.svg" width={32} height={32} />
+          <Image src="/skill-js.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-ts.svg" width={32} height={32} />
+          <Image src="/skill-ts.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-react.svg" width={32} height={32} />
+          <Image src="/skill-react.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-nextjs.svg" width={32} height={32} />
+          <Image src="/skill-nextjs.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-tailwindcss.svg" width={32} height={32} />
+          <Image src="/skill-tailwindcss.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-mantine.svg" width={32} height={32} />
+          <Image src="/skill-mantine.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-solidity.svg" width={32} height={32} />
+          <Image src="/skill-solidity.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-ethers.svg" width={32} height={32} />
+          <Image src="/skill-ethers.svg" width={42} height={42} />
         </li>
         <li>
-          <Image src="/skill-hardhat.svg" width={32} height={32} />
+          <Image src="/skill-hardhat.svg" width={42} height={42} />
         </li>
       </ul>
     </section>
