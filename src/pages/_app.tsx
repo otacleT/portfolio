5fb9292c8416f6273174727a5431380a5bb722f2
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 14:16:22 +0900

    ✨ use mantineProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "src/components/Header";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MantineProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </MantineProvider>
   );
 }
 
