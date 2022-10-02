ae9fef5a18ba09acaf332dbaa8dec39c0a4479a2
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 21:47:20 +0900

    💄 メニューボタンのUI調整

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,47 @@
-import { Menu, Text } from "@mantine/core";
 import { MoonIcon } from "@heroicons/react/24/solid";
+import { Bars3CenterLeftIcon } from "@heroicons/react/24/outline";
+import { Menu } from "@mantine/core";
+import { useState } from "react";
+import Link from "next/link";
 
 export const Header = () => {
+  const [opened, setOpened] = useState<boolean>(false);
   return (
     <div className="max-w-2xl mx-auto px-4 flex items-center justify-between">
-      <Menu shadow="md" width={200}>
+      <Menu
+        width={220}
+        radius="md"
+        position="bottom-start"
+        onOpen={() => setOpened(true)}
+        onClose={() => setOpened(false)}
+        closeOnItemClick={true}
+      >
         <Menu.Target>
-          <button className="relative w-6 h-6 before:content-[''] before:w-full before:h-[1px] before:absolute before:left-0 before:top-1 before:bg-[#202020] after:content-[''] after:w-full after:h-[1px] after:absolute after:left-0 after:bottom-1 after:bg-[#202020]">
-            <span className="w-4 h-[1px] bg-[#202020] absolute t-1/2 -translate-y-1/2 left-0"></span>
+          <button
+            className={`h-10 w-10 flex items-center justify-center rounded-md hover:bg-[#E6F0EB] ${
+              opened && "border border-[#DCE6E1] bg-[#E6F0EB]"
+            }`}
+          >
+            <Bars3CenterLeftIcon className="w-9 h-7" />
           </button>
         </Menu.Target>
 
         <Menu.Dropdown>
-          <Menu.Item>Home</Menu.Item>
-          <Menu.Item>Works</Menu.Item>
-          <Menu.Item>Posts</Menu.Item>
+          <Menu.Item
+            className="hover:decoration-1 hover:underline"
+            component="a"
+            href="/"
+          >
+            Home
+          </Menu.Item>
+          <Menu.Item
+            className="hover:decoration-1 hover:underline"
+            component="a"
+            href="/works"
+          >
+            Works
+          </Menu.Item>
+          <p className="block px-3 py-2 text-sm text-[#888888]">coming soon</p>
         </Menu.Dropdown>
       </Menu>
       <button className="h-10 w-10 rounded-md bg-[#805AD5] flex items-center justify-center">
