1742fe1e4bbf2da171c951bceb02e00bffd77a42
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 20:28:05 +0900

    ✨ Footerコンポーネント

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className="max-w-xl mx-auto py-6">
+      <p className="text-xs text-center">
+        © 2022 Taisei Miyabe. All Rights Reserved.
+      </p>
+    </div>
+  );
+};
