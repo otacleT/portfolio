70dbcd78143baa58851e83ca5d48735992062914
MIYABETaisei <mytspe46@gmail.com>
2022-10-02 14:45:19 +0900

    ♻️ Bioコンポーネント

---
+++ src/components/Bio/Bio.tsx
@@ -0,0 +1,17 @@
+export const Bio = () => {
+  return (
+    <section className="section-wrap mt-28">
+      <h2 className="section-title">Bio</h2>
+      <ul className="[&>li]:text-sm mt-3">
+        <li>
+          <span className="font-semibold pr-3">2022.3</span>
+          九州工業大学情報工学部　卒業
+        </li>
+        <li className="mt-3">
+          <span className="font-semibold pr-3">2022.4</span>
+          九州工業大学情報工学府　入学
+        </li>
+      </ul>
+    </section>
+  );
+};
