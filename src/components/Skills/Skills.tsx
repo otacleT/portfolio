6523d26832019b9f29c28cac8cfc0db7cc74381b
MIYABETaisei <mytspe46@gmail.com>
2022-10-03 21:40:25 +0900

    💄 技術スタックの画像サイズ調整

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,7 +4,7 @@ export const Skills = () => {
   return (
     <section className="section-wrap">
       <h2 className="section-title">Skills</h2>
-      <ul className="flex items-center space-x-1 sm:space-x-2 space-y-1 sm:space-y-2 mt-3 flex-wrap [&>li]:w-8 sm:[&>li]:w-10 [&>li]:h-8 sm:[&>li]:h-10">
+      <ul className="flex items-center space-x-1 sm:space-x-2 space-y-2 mt-3 flex-wrap [&>li]:w-9 sm:[&>li]:w-10 [&>li]:h-9 sm:[&>li]:h-10">
         <li>
           <Image alt="html5" src="/skill-html5.svg" width={42} height={42} />
         </li>
