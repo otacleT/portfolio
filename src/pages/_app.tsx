d95f36426b09133b8647788ce3131df6b82a5e06
MIYABETaisei <mytspe46@gmail.com>
2022-10-03 21:10:17 +0900

    ⚡️ fetcherをグローバルに定義

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,19 @@ import { Header } from "src/components/Header";
 import { MantineProvider } from "@mantine/core";
 import { Footer } from "src/components/Footer";
 import { ThemeProvider } from "next-themes";
+import { SWRConfig } from "swr";
+import { fetcher } from "src/lib/fetcher";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <MantineProvider>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </MantineProvider>
+      <SWRConfig value={{ fetcher }}>
+        <MantineProvider>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </MantineProvider>
+      </SWRConfig>
     </ThemeProvider>
   );
 }
