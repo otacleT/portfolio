404eab280c3cb28b1ab688f6b893b2750668be2c
MIYABETaisei <mytspe46@gmail.com>
2022-10-03 17:45:02 +0900

    💄 上の余白調整

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="max-w-xl mx-auto py-6">
+    <div className="max-w-xl mx-auto pt-10 pb-6">
       <p className="text-xs text-center dark:text-[#F5FFFA]">
         © 2022 Taisei Miyabe. All Rights Reserved.
       </p>
