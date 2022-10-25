678d70866c1dbc35383f2434f5ee5c52b177506c
MIYABETaisei <mytspe46@gmail.com>
2022-10-25 17:37:52 +0900

    ✨ add portfolio

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -33,7 +33,7 @@ const Works: NextPage = () => {
         </Link>
       </div>
       <h3 className="section-subTitle mt-8">- Web app</h3>
-      <div className="flex flex-wrap justify-between items-start space-y-6 sm:space-y-0 mt-3">
+      <div className="flex flex-wrap justify-between items-start mt-3">
         <Link href="/works/colorful-todo">
           <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
@@ -54,7 +54,7 @@ const Works: NextPage = () => {
           </a>
         </Link>
         <Link href="/works/no-spoilers-twitter">
-          <a className="w-full sm:w-[calc(50%-5px)] block">
+          <a className="w-full sm:w-[calc(50%-5px)] block mt-6 sm:mt-0">
             <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
               <Image
                 className="object-cover"
@@ -69,6 +69,25 @@ const Works: NextPage = () => {
             </h4>
             <p className="dark:text-[#F5FFFA] text-xs sm:text-sm text-center mt-1">
               Twitterによるネタバレを防ぐアプリです。
+            </p>
+          </a>
+        </Link>
+        <Link href="/works/portfolio">
+          <a className="w-full sm:w-[calc(50%-5px)] block mt-6 sm:mt-5">
+            <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
+              <Image
+                className="object-cover"
+                alt="ポートフォリオサイトの画像"
+                src="/portfolio-top.JPG"
+                width={600}
+                height={360}
+              />
+            </div>
+            <h4 className="dark:text-[#F5FFFA] text-[14px] sm:text-[15px] font-semibold text-center mt-3">
+              ポートフォリオサイト
+            </h4>
+            <p className="dark:text-[#F5FFFA] text-xs sm:text-sm text-center mt-1">
+              今ご覧になっているWebサイトです。
             </p>
           </a>
         </Link>
