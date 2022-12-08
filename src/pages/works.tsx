8f6249b999bf2491e849f8e516c4cba33836963f
MIYABETaisei <mytspe46@gmail.com>
2022-12-08 09:03:51 +0900

    🎨 位置修正

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -77,25 +77,6 @@ const Works: NextPage = () => {
             </p>
           </a>
         </Link>
-        <Link href="/works/portfolio">
-          <a className="w-full sm:w-[calc(50%-5px)] block mt-6 sm:mt-5">
-            <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
-              <Image
-                className="object-cover"
-                alt="ポートフォリオサイトの画像"
-                src="/portfolio-top.jpg"
-                width={600}
-                height={360}
-              />
-            </div>
-            <h4 className="dark:text-[#F5FFFA] text-[14px] sm:text-[15px] font-semibold text-center mt-3">
-              ポートフォリオサイト
-            </h4>
-            <p className="dark:text-[#F5FFFA] text-xs sm:text-sm text-center mt-1">
-              今ご覧になっているWebサイトです。
-            </p>
-          </a>
-        </Link>
         <Link href="/works/scroll-game">
           <a className="w-full sm:w-[calc(50%-5px)] block mt-6 sm:mt-5">
             <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
@@ -117,7 +98,7 @@ const Works: NextPage = () => {
         </Link>
       </div>
       <h3 className="section-subTitle mt-8">- Web page</h3>
-      <div className="flex flex-wrap justify-between items-start space-y-6 sm:space-y-0 mt-3">
+      <div className="flex flex-wrap justify-between items-start mt-3">
         <Link href="/works/kyuko">
           <a className="w-full sm:w-[calc(50%-5px)] block">
             <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
@@ -153,6 +134,25 @@ const Works: NextPage = () => {
             </h4>
             <p className="dark:text-[#F5FFFA] text-xs sm:text-sm text-center mt-1">
               フロント部分のコーディングを行いました。
+            </p>
+          </a>
+        </Link>
+        <Link href="/works/portfolio">
+          <a className="w-full sm:w-[calc(50%-5px)] block mt-6 sm:mt-5">
+            <div className="w-full aspect-video rounded-md overflow-hidden relative shadow-[0_0_0_2px_rgba(230,240,235,1)] dark:shadow-[0_0_0_2px_rgba(70,70,70,1)]">
+              <Image
+                className="object-cover"
+                alt="ポートフォリオサイトの画像"
+                src="/portfolio-top.jpg"
+                width={600}
+                height={360}
+              />
+            </div>
+            <h4 className="dark:text-[#F5FFFA] text-[14px] sm:text-[15px] font-semibold text-center mt-3">
+              ポートフォリオサイト
+            </h4>
+            <p className="dark:text-[#F5FFFA] text-xs sm:text-sm text-center mt-1">
+              今ご覧になっているWebサイトです。
             </p>
           </a>
         </Link>
