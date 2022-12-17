b55dd2c2978d23e4066e86c4650670996c512831
MIYABETaisei <mytspe46@gmail.com>
2022-12-17 15:41:14 +0900

    ⚡️ eslintのtypescript設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,34 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "prettier",
-  ],
+  plugins: ["sort-keys-custom-order"],
   rules: {
+    "no-console": ["error", { allow: ["warn", "info", "error"] }],
     "no-undef": "error",
-    "react/prop-types": "off",
     "react/jsx-props-no-spreading": "off",
+    "react/prop-types": "off",
+    "sort-keys-custom-order/object-keys": [
+      "error",
+      { orderedKeys: ["id", "name", "title"] },
+    ],
+    "sort-keys-custom-order/type-keys": [
+      "error",
+      { orderedKeys: ["id", "name", "title"] },
+    ],
   },
   settings: {
     react: {
